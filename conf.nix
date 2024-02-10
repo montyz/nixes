eb5313c11d67cb35976ad4b54913b058f7fb3514
Monty Zukowski <monty@codetransform.com>
2024-02-10 09:37:19 -0800

    Update conf.nix 1Password

--- conf.nix
+++ conf.nix
@@ -137,6 +137,13 @@ services.openssh = {
   #settings.PermitRootLogin = "yes";
 };
 services.tailscale.enable = true;
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "yourUsernameHere" ];
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
