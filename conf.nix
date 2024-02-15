6ea9cb787df40cb6c5c2831a4300f15c25d9e3a5
Monty Zukowski <monty@washdish.com>
2024-02-15 10:37:40 -0800

    updates

--- conf.nix
+++ conf.nix
@@ -162,5 +162,6 @@ services.tailscale.enable = true;
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
