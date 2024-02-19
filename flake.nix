3297f5cdca491b7230ccf02c4ef9e9b99f3812ca
Monty Zukowski <monty@washdish.com>
2024-02-19 09:28:18 -0800

    agenix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             {
                 environment.systemPackages = [ 
                     agenix.packages.x86_64-linux.default 
-                    pkgs.kitty
+                    nixpkgs.kitty
                 ];
             }
           ];
