497d0616a4554dbeafae5c69387fdf7a6c440c17
Monty Zukowski <monty@washdish.com>
2024-02-20 08:41:53 -0800

    agenix

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,6 +1,18 @@
 { self, config, lib, pkgs, ... }: {
   # Based on https://carjorvaz.com/posts/the-holy-grail-nextcloud-setup-made-easy-by-nixos/
   services = {
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+        };
+    };
     nextcloud = {
       enable = true;
       hostName = "nextcloud.home";
