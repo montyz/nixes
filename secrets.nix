87f7814b31858874dbef24cb32b811dc579bdc46
Monty Zukowski <monty@washdish.com>
2024-03-01 10:17:48 -0800

    updates

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,7 @@
 let
   monty = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAEof1VUD1hzvK6u6SC3/oKFqMyGKCVSiA1SXnx8RQn";
-  users = [ monty ];
+  montynix0 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPeA3INkmDWVBlb3Dy5wyue8uSLBZliZG/lU0u5e7fD/";
+  users = [ monty montynix0 ];
 
   nix0 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGMySVZTV4gULvP6UPYZrxDDuxlCO7Z6CHR2FpijrR0i";
   systems = [ nix0 ];
