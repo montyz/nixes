e4329b25792aed21079f00625cc0bc49537cbbc0
Monty Zukowski <monty@washdish.com>
2024-03-27 10:12:01 -0700

    up

--- nixos/nextcloud.nix
+++ nixos/nextcloud.nix
@@ -1,15 +1,15 @@
 { self, config, lib, pkgs, agenix, ... }: {
-  agenix.nixosModules.default
+  agenix.nixosModules.default =
     {
         environment.systemPackages = [ 
             agenix.packages.x86_64-linux.default 
         ];
-    }
-    {
+    };
+    
       age.secrets.nextcloud_admin_pass.file = ./nextcloud_admin_pass.age;
       age.secrets.nextcloud_admin_pass.owner = "nextcloud";
       age.secrets.easydns.file = ./easydns.age;
-    }
+    
   security.acme = {
     acceptTerms = true;
     defaults = {
