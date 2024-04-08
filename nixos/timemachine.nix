1047b31ec15832e26d4f3ae0a5edb8d16bc6a786
Monty Zukowski <monty@washdish.com>
2024-04-08 10:44:54 -0700

    avahi fix 2?

--- nixos/timemachine.nix
+++ nixos/timemachine.nix
@@ -19,7 +19,7 @@
 
         avahi = {
             enable = true;
-            nssmdns = false;
+            nssmdns = true;
 
             publish = {
                 enable = true;
@@ -27,10 +27,5 @@
             };
         };
     };
-    # this is what avahi.nssmdns does, but mdns4 (IPv4) instead of mdns (dual-stack)
-    system.nssModules = pkgs.lib.optional true pkgs.nssmdns;
-    system.nssDatabases.hosts = pkgs.lib.optionals true (pkgs.lib.mkMerge [
-        (pkgs.lib.mkBefore [ "mdns4_minimal [NOTFOUND=return]" ]) # before resolve
-        (pkgs.lib.mkAfter [ "mdns4" ]) # after dns
-    ]);
+    services.nscd.enableNsncd = true;
 }
