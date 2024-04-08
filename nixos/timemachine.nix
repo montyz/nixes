c14ca3b60d1a2693eaff38544dfa612b1606ac0f
Monty Zukowski <monty@washdish.com>
2024-04-08 10:31:59 -0700

    avahi fix?

--- nixos/timemachine.nix
+++ nixos/timemachine.nix
@@ -1,29 +1,36 @@
 { self, config, lib, pkgs, ... }: {
     networking.firewall.allowedTCPPorts = [
     548 # netatalk
+    636
     ];
 
     services = {
-    netatalk = {
-        enable = true;
+        netatalk = {
+            enable = true;
 
-        volumes = {
-        "monty-time-machine" = {
-            "time machine" = "yes";
-            path = "/home/monty/time-machine";
-            "valid users" = "monty";
+            volumes = {
+            "monty-time-machine" = {
+                "time machine" = "yes";
+                path = "/home/monty/time-machine";
+                "valid users" = "monty";
+            };
+            };
         };
+
+        avahi = {
+            enable = true;
+            nssmdns = false;
+
+            publish = {
+                enable = true;
+                userServices = true;
+            };
         };
     };
-
-    avahi = {
-        enable = true;
-        nssmdns = true;
-
-        publish = {
-        enable = true;
-        userServices = true;
-        };
-    };
-    };
+    # this is what avahi.nssmdns does, but mdns4 (IPv4) instead of mdns (dual-stack)
+    system.nssModules = pkgs.lib.optional true pkgs.nssmdns;
+    system.nssDatabases.hosts = pkgs.lib.optionals true (pkgs.lib.mkMerge [
+        (pkgs.lib.mkBefore [ "mdns4_minimal [NOTFOUND=return]" ]) # before resolve
+        (pkgs.lib.mkAfter [ "mdns4" ]) # after dns
+    ]);
 }
