04dd6026d53dba158a3ed097b718031864362bfe
Monty Zukowski <monty@washdish.com>
2024-04-11 10:08:37 -0700

    another option

--- nixos/timemachine.nix
+++ nixos/timemachine.nix
@@ -20,7 +20,7 @@
         avahi = {
             enable = true;
             nssmdns = true;
-
+            openFirewall = true;
             publish = {
                 enable = true;
                 userServices = true;
