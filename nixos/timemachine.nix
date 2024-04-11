e569bc01c51075646fccc5264c04697a108111ac
Monty Zukowski <monty@washdish.com>
2024-04-11 10:14:59 -0700

    again

--- nixos/timemachine.nix
+++ nixos/timemachine.nix
@@ -24,8 +24,14 @@
             publish = {
                 enable = true;
                 userServices = true;
+                addresses = true;
             };
         };
     };
-    services.nscd.enableNsncd = true;
+    system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+    system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+      (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+      (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+    ]);
+    #services.nscd.enableNsncd = true;
 }
